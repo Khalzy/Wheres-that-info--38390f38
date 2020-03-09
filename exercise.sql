aebb0212309f7b5cb29d1a8347d3b93c2e46bb9e
Candace <2100978@talnet.nl>
2020-03-09 11:35:34 +0100

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,13 +1,65 @@
-
-    
-    SELECT 
-    id,
+Select
 	title,
-	rating,
-	description,
-	has_won_awards,
-	seasons,
-	country,
-	language
-    FROM series;
-    SELECT * FROM series;
+    has_won_awards
+    FROM 
+    series
+    WHere has_won_awards = 1
+    ;
+    
+Select
+    title,
+    rating
+    FROM 
+    series
+    WHERE rating > 2.5;
+        
+Select
+    title,
+    country,
+    language
+    From series
+    WHERE country = "NL" AND
+    language = "NL";
+        
+        
+Select
+    title,
+    seasons
+
+    From series
+    WHERE seasons < 5;
+            
+Select
+    title,
+    MAX(rating) as rating 
+            
+    From series
+    WHERE rating
+;
+
+
+Select
+    title,
+    seasons
+            
+    From series
+    WHERE seasons < 3 or seasons > 20;
+
+Select
+    title
+
+            
+    From series
+    WHERE title LIKE '%Th%'
+;
+
+Select
+    title,
+    seasons
+            
+    From series
+    WHERE seasons != 3 ;
+
+
+
+select * from series;
